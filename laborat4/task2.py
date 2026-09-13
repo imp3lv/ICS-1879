26c22b72e61e58c4e7604f5251b8dd1ba329acd4
imp3lv <reynhiik@gmail.com>
2026-09-13 22:46:23 +0300

    Add raxyemo function for complex calculations

--- laborat4/task2.py
+++ laborat4/task2.py
@@ -1,1 +1,16 @@
+import math as m
 
+def raxyemo(a, b, c, d):
+    x1 = a**2
+    x2 = m.log(abs(b))
+    x3 = m.sin(c) * m.sqrt(abs(d))
+    result = x1 + x2 - x3
+    return result
+
+a = float(input("Введіть x: "))
+b = float(input("Введіть a: "))
+c = float(input("Введіть b: "))
+d = float(input("Введіть c: "))
+
+result = raxyemo(a, b, c, d)
+print("result=", result)
