8727aa22e306d185e5704875a77e4a8776b4ff72
imp3lv <reynhiik@gmail.com>
2026-09-17 01:49:46 +0300

    Add mathematical function selection based on input

--- laborat5/task1.py
+++ laborat5/task1.py
@@ -1,1 +1,12 @@
+import math as m
 
+x = float(input("Введіть x: "))
+
+if x >= 3:
+    result = m.sin(x)
+elif x >= 0 and x < 3:
+    result = m.cos(x)
+else:
+    result = m.tan(x)
+
+print("Вiдповiдь:", result)
